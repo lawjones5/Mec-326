ba600e89244bbdaee84e6389c538ab3b3f5bdabe
Lawrence <ljones20@sheffield.ac.uk>
2022-11-08 20:25:56 +0000

    Legend on Graph

--- Mec326.m
+++ Mec326.m
@@ -2,25 +2,33 @@ load("blade1_data.mat");
 
 tS = point(4).time(:,2);
 
-sf = size(tS)./abs(max(tS));
+
+sf = length(tS)./max(tS);
 
 for points = 1:10
 
     tempoutput = mean(point(points).output(:,:),2);
 %     plot(point(points).time(:,1),tempoutput);
     hold on
-    legend
+    legendstrings{points}=sprintf('%s %d','Point ',points);
+    
+
+
+
     FRF = fft(tempoutput());
     FRF = [ FRF(1) ; 2*FRF(2:end/2) ];
 
+%     freq = (0:sf/size(FRF):sf);
+%     freq = freq';
+%     freq = freq(2:end) ;
+    
+    k= (0:(length(FRF)-1))';
+    w=k*sf./length(FRF);
 
-    size(FRF)
-    freq = (0:sf/size(FRF):sf);
-    freq = freq';
-    freq = freq(2:end) ;
 
     set(gca, 'YScale', 'log')
-    plot(freq(1:end),abs(FRF))
-   
+    plot(w,abs(FRF))
+    hold off
 
 end
+legend(legendstrings)
