e3e30889c98176edfe93a3328e95b9650d9e3100
Lawrence <ljones20@shef.ac.uk>
2022-11-11 13:51:15 +0000

    Auto stash before merge of "main" and "origin/main"

--- Mec326.m
+++ Mec326.m
@@ -10,24 +10,28 @@ sf = length(tS)./max(tS);
 
 for points = 1:10
     
-    tempoutput = mean(point(points).output(:,:),2);
+%     tempoutput = mean(point(points).output(:,:),2);
     hold on
-    legendstrings{points}=sprintf('%s %d','Point ',points);
 
+    tempoutput = point(points).output(:,:);
+%     legendstrings{points}=sprintf('%s %d','Point ',points);
 
-    FRF = fft(tempoutput());
+    N = length(tempoutput);
+
+    tempFRF = fft(tempoutput())/N;
+    FRF = mean(tempFRF,2);
+    
     FRF = [ FRF(1) ; 2*FRF(2:end/2) ];
 
-
-    k= (0:(length(FRF)-1))';
-    w=k*sf./length(FRF);
-
+%     k= (0:(N-1))';
+%     w=k*sf./N;
+    w=(0:(N/2-1))'*sf/N;
 
     set(gca, 'YScale', 'log')
     %     plot(w,abs(FRF))
     xlim([0 1000])
 
-
+% FINDING PEAKS
     [pks,locs] = findpeaks(abs(FRF),'MinPeakDistance',100);
     
 
@@ -41,4 +45,4 @@ for points = 1:10
 
 
 end
-legend(legendstrings)
+% legend(legendstrings)
