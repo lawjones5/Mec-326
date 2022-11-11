a747b50fe80b021ae364109932888c2ff93e591b
Lawrence <ljones20@sheffield.ac.uk>
2022-11-11 12:31:06 +0000

    Find Peaks

--- Mec326.m
+++ Mec326.m
@@ -1,3 +1,6 @@
+clear all
+clear figure
+
 load("blade1_data.mat");
 
 tS = point(4).time(:,2);
@@ -6,29 +9,36 @@ tS = point(4).time(:,2);
 sf = length(tS)./max(tS);
 
 for points = 1:10
-
+    
     tempoutput = mean(point(points).output(:,:),2);
-%     plot(point(points).time(:,1),tempoutput);
     hold on
     legendstrings{points}=sprintf('%s %d','Point ',points);
-    
-
 
 
     FRF = fft(tempoutput());
     FRF = [ FRF(1) ; 2*FRF(2:end/2) ];
 
-%     freq = (0:sf/size(FRF):sf);
-%     freq = freq';
-%     freq = freq(2:end) ;
-    
+
     k= (0:(length(FRF)-1))';
     w=k*sf./length(FRF);
 
 
     set(gca, 'YScale', 'log')
-    plot(w,abs(FRF))
-    hold off
+    %     plot(w,abs(FRF))
+    xlim([0 1000])
+
+
+    [pks,locs] = findpeaks(abs(FRF),'MinPeakDistance',100);
+    
+
+    [pks,locs] = findpeaks(pks,locs);
+    w_peak= w(locs);
+
+    plot(w,abs(FRF),w(locs),pks,'or')
+
+
+
+
 
 end
 legend(legendstrings)
